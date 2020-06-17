6d17835e5c97df6f97b2537c8de23de311f2dd3a
JustTeRoR <pavlenok2000@gmail.com>
2020-06-17 23:03:05 +0300

    Created activity and layput file for further developing of login section. Also refactored detail view a little.

---
+++ app/src/main/java/com/haerful/foodapp/view/login/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.haerful.foodapp.view.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.haerful.foodapp.R;
+
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+
+    }
+}
